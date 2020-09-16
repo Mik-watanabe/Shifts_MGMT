774200457a3de492fdcf363a33a21839bab6d1e2
Miku Watanabe <laugh.luv919@gmail.com>
2020-09-16 20:09:16 +0900

    shift_mgmtのbase

---
+++ database/migrations/2020_09_15_130134_create_shifts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->INTEGER('id');
+            $table->INTEGER('user_id');
+            $table->DATE('shift_date');
+            $table->DATETIME('shift_start');
+            $table->DATETIME('shift_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shifts');
+    }
+}
