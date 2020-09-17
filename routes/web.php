6714f3fd015f9634c879250a213a0e4641738432
Miku Watanabe <laugh.luv919@gmail.com>
2020-09-17 11:36:28 +0900

    {id}/homeと、Index.blade.php,UserControllerをつなげた

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/{id}/home', 'UserController@index')->name('users.index');
+Route::get('/{id}/home', 'UserController@index');
