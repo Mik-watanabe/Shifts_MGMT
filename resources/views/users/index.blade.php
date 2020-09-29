a8d1c3eefed609b40b7238a9f9a336e205b1eba7
Miku Watanabe <laugh.luv919@gmail.com>
2020-09-29 15:59:13 +0900

    カレンダーを表示

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.app')
 
-@section('content')
-{{ $user->id}}
 
+@section('content')
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.3.2/main.min.css">
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.3.2/main.min.js"></script>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth'
+  });
+  calendar.render();
+});
+</script>
+
+<div id='calendar'></div>
 @endsection
