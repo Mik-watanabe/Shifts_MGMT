efdf954d55d42c990e88c775614982dc2f1d2070
Miku Watanabe <laugh.luv919@gmail.com>
2020-10-03 15:36:37 +0900

    見た目

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,10 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+    public function index(Request $request)
+    {   
+       
+        $user = Auth::user();   
+        return view('home', ['user' => $user]);
     }
 }
