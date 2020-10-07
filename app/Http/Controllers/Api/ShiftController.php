4fbfb643e52bc3f0bd4433d64b4ad06b1218a138
Miku Watanabe <laugh.luv919@gmail.com>
2020-10-07 17:36:07 +0900

    パスワードのHash化

--- app/Http/Controllers/Api/ShiftController.php
+++ app/Http/Controllers/Api/ShiftController.php
@@ -8,21 +8,10 @@ use App\Models\Shift;
 
 class ShiftController extends Controller
 {
-    //
     public function index(Request $request) 
    {
-    //     Shift::create([
-    //         "user_id" => Auth::user()->id,
-    //         "shift_date" => $request->date,
-    //         "shift_start" => $request->start,
-    //         "shift_end" => $request->end
+        $shifts = Shift::all();
 
-    //     ]);
-    $shifts = Shift::all();
-         return view('users/shifts',[
-             'shifts' => $shifts
-         ]);
-        // return response()->json($request->get('start'));
-        
+        return response()->json($shifts);
     }
 }
