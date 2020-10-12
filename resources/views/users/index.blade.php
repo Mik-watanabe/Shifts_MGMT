c4aa298b5b45924881fbadc2f8b097ed1c6b0d9b
Miku Watanabe <laugh.luv919@gmail.com>
2020-10-12 16:28:56 +0900

    promptの追加

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,12 +8,10 @@
 <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
 
 <script>
-
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    // plugins: [ 'interactionPlug'],
     initialView: 'dayGridMonth',
     selectable: true,
     editable: true,
@@ -21,8 +19,6 @@ document.addEventListener('DOMContentLoaded', function() {
     displayEventEnd: true,
     
     events: "/api/shifts",
-
-    timeFormat: 'H(:mm)',
 
       
     // eventDrop: function(info){
@@ -36,11 +32,6 @@ document.addEventListener('DOMContentLoaded', function() {
     
       addEvent(calendar,info);
     },
-
-    // dateClick: function(info){
-    //   let title = prompt("希望の勤務時間を入力してください:");
-     
-    //   info.dayEl.innerText = title;
 
     
   });
@@ -71,6 +62,7 @@ function addEvent(calendar,info) {
             }
           });
 }
+
 </script>
 
 <div id='calendar'></div>
