728cbd9c9de500d90c5e6db3548d4ad085113a84
Miku Watanabe <laugh.luv919@gmail.com>
2020-10-25 16:04:22 +0900

    registerのダイレクト先のviewの変更

--- resources/views/manager/thanks.blade.php
+++ resources/views/manager/thanks.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"> <i class="fas fa-check" style="color:#77bfa3;"></i>&nbsp;登録完了</div>
+                <div class="card-header"> <i class="fas fa-check" style="color:#77bfa3;"></i>&nbsp;送信完了</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,9 +13,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <img class="image" src="{{ asset('img/undraw_confirmed_81ex.png') }}" alt="invite-img">
-                    <P style="text-align: center;">ありがとうございます。<br>
-                    登録が完了しました。これから一緒にがんばっていきましょう☺️</P>
+                    <img class="image" src="{{ asset('img/undraw_Delivery_re_f50b.png') }}" alt="invite-img">
+                    <P style="text-align: center;">新スタッフへ招待メールの送信が完了しました。</P>
 
                 </div>
             </div>
@@ -23,4 +22,3 @@
     </div>
 </div>
 @endsection
-
